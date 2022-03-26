ce3098f538d9a4fad6fa289d59640c347457ee94
Sofía M. Delgado Balaguera <91962122+s1085496@users.noreply.github.com>
2022-03-26 19:26:52 +0100

    SofiasFile
    Hello Dariya! Please accept this commit :)

---
+++ SofiasFile.py
@@ -0,0 +1,6 @@
+# Hello Dariya, it's Sofia here!
+# Nice quote -> "Life is either a great adventure or nothing." by Helen Keller.
+# I don't like spicy food.
+# Book Recommendation: Siddhartha by Hermann Hesse -> My favorite german writer.
+# German Word: "Fluss" which means river.
+# Regards!
